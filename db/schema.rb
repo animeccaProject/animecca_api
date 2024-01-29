b8883531e2dc45c2e3535ffe31ae8536af9e7734
haga_hiroki <ragna.1123@gmail.com>
2024-01-29 17:44:15 +0900

    userUniqの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_29_082250) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_29_084023) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "mecca_id", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_29_082250) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "favorites", "meccas", on_delete: :cascade
