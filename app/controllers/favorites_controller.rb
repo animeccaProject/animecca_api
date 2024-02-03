6d7fb234ee9f018e5b98f18a240d708458d1925e
haga_hiroki <ragna.1123@gmail.com>
2024-02-03 13:35:24 +0900

    favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,25 +1,42 @@
 # frozen_string_literal: true
 
 class FavoritesController < ApplicationController
-  before_action :jwt_authenticate, only: %i[create destroy]
+  before_action :jwt_authenticate, only: %i[index create destroy]
+
+  def index
+    fovorites = @user.favorites
+    meccas = fovorites.map { |favorite| favorite.mecca }
+    if fovorites.nil?
+      render json: { error: 'お気に入りが見つかりません' }, status: :not_found
+      return
+    end
+
+    render json: meccas
+  end
 
   def create
-    @favorite = @user.favorites.new(mecca_id: params[:mecca_id])
-    if @favorite.save
-      render json: @favorite
+    favorite = @user.favorites.new(mecca_id: params[:mecca_id])
+    if favorite.save
+      render json: favorite
     else
-      render json: @favorite.errors, status: :unprocessable_entity
+      render json: favorite.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @mecca = Mecca.find(params[:mecca_id])
-    @favorite = @user.favorites.find_by(mecca_id: @mecca.id)
+    favorite = @user.favorites.find_by(mecca_id: params[:mecca_id])
 
-    if @favorite.destroy
-      render json: @favorite, status: :ok
-    else
-      render json: @favorite.errors, status: :unprocessable_entity
+    if favorite.nil?
+      render json: { error: 'お気に入りが見つかりません' }, status: :not_found
+      return
+    end
+  
+    begin
+      favorite.destroy!
+      render json: { message: 'お気に入りを解除しました' }
+    rescue ActiveRecord::RecordNotDestroyed
+      render json: favorite.errors, status: :unprocessable_entity
     end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
+  # users関係
   resources :users, only: [:create] do
     collection do
       post '/login', to: 'users#login'
     end
   end
 
+  # 階層が一個上がるので、meccasのルーティングから外しました
+  # (mecca_idを指定しないルーティングにしました)
+  get '/favorites', to: 'favorites#index'
+
   resources :meccas, only: [:show, :create, :update, :destroy] do
-    resources :favorites, only: [:create, :destroy ]
+    # favorite_idが末尾につくので、favorite_idを指定しないルーティングにしました
+    post '/favorites', to: 'favorites#create'
+    delete '/favorites', to: 'favorites#destroy'
+    # prefecture
     collection do
       get '/prefecture/:prefecture', to: 'meccas#prefecture'
     end
