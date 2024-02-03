5db84ba55b90e77e3f5ed74c8eed9e8f2a193c08
haga_hiroki <ragna.1123@gmail.com>
2024-02-03 11:42:55 +0900

    アップローダーの作成

--- app/controllers/meccas_controller.rb
+++ app/controllers/meccas_controller.rb
@@ -3,7 +3,30 @@
 class MeccasController < ApplicationController
   before_action :jwt_authenticate, only: %i[create update]
 
-  # 聖地の一覧表示
+  #  聖地の詳細表示
+  def show
+    mecca_params_id = params.permit(:id)
+    mecca = Mecca.find(mecca_params_id[:id])
+
+    if mecca.present?
+      render json: mecca, include: :images
+    else
+      render json: { error: 'お探しのIDの聖地は見つかりませんでした' }, status: :not_found
+    end
+  end
+
+  # 聖地を都道府県からの検索
+  def prefecture
+    mecca_params = params.permit(:prefecture)
+    meccas = Mecca.where(prefecture: mecca_params[:prefecture])
+    if meccas
+      render json: meccas, include: :images
+    else
+      render json: { error: 'お探しの都道府県では聖地は見つかりませんでした' }, status: :not_found
+    end
+  end
+
+  # 聖地新規登録
   def create
     mecca_data = JSON.parse(params[:mecca])
     mecca = Mecca.new(mecca_data['mecca'])
@@ -11,25 +34,27 @@ class MeccasController < ApplicationController
   
     if mecca.valid?
 
-      if mecca.save
-        render json: mecca
-      else
-        render json: mecca.errors, status: :unprocessable_entity
-      end
       
       # 画像データがある場合、処理を行う
-      image_data = image_params
-      if image_data.present?
+      images = image_params
+      if images.present?
         begin
-          image_store(mecca, image_data)
+          image_store(mecca, images)
         rescue StandardError => e
           mecca.errors.add(:image, e.message)
         end
       end
-
+      
+      if mecca.save
+        render json: mecca, include: :images, status: :created
+      else
+        render json: mecca.errors, status: :unprocessable_entity
+      end
+      
     else
       render json: mecca.errors, status: :unprocessable_entity
     end
+
   end
   
 
@@ -73,26 +98,17 @@ class MeccasController < ApplicationController
   end
 
   def image_params
-    params.require(:image)
+    params.require(:images)
   end
 
-  def image_store(mecca_data, image_data)
-    # 保存先ディレクトリの確認と作成
-    FileUtils.mkdir_p(Rails.root.join('public', 'images', mecca_data.id.to_s))
-
-    # ファイルネームの設定
-    filename = "#{Time.zone.now.to_i}_#{SecureRandom.hex(10)}_#{image_data.original_filename}"
-
-    # 画像の保存パスの設定
-    file_path = Rails.root.join('public', 'images', mecca_data.id.to_s,
-                                filename)
-
-    # 画像の保存
-    File.open(file_path, 'wb') do |file|
-      file.write(image_data.read)
+  def image_store(mecca, images)
+    images.each do |image_file|
+      # 保存先のディレクトリを設定
+  
+      # 画像の保存処理（例：CarrierWaveを使用）
+      image = mecca.images.new
+      image.path = image_file
+      image.save
     end
-
-    # オプション: 画像パスをデータベースに保存
-    mecca_data.image_path = file_path.relative_path_from(Rails.root.join('public'))
   end
 end
